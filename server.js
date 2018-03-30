cc818e22b9b9de767679f1ce39146d5e3a116148
Gene Huh <ehuh@teamexos.com>
2018-03-30 14:56:22 -0700

    upgraded example to latest apollo engine version

--- server.js
+++ server.js
@@ -2,16 +2,34 @@ import express from 'express';
 import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
 import bodyParser from 'body-parser';
 import schema from './data/schema';
+import compression from 'compression';
+import { ApolloEngine } from 'apollo-engine';
+
+const app = express();
 
 const GRAPHQL_PORT = 3000;
+const ENGINE_API_KEY = 'service:genehuh39-7620:D2s8aMf905zj259E-xEelw';
 
-const graphQLServer = express();
+const engine = new ApolloEngine({
+  apiKey: ENGINE_API_KEY
+});
 
-graphQLServer.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
-graphQLServer.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
 
-graphQLServer.listen(GRAPHQL_PORT, () =>
-  console.log(
-    `GraphiQL is now running on http://localhost:${GRAPHQL_PORT}/graphiql`
-  )
-);
+app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
+app.use(compression());
+app.use('/graphql', bodyParser.json(), graphqlExpress({
+    schema,
+    // This option turns on tracing
+    tracing: true
+}));
+
+engine.listen({
+    port: GRAPHQL_PORT,
+    graphqlPaths: ['/api/graphql'],
+    expressApp: app,
+    launcherOptions: {
+        startupTimeout: 3000,
+    },
+}, () => {
+    console.log('Listening!');
+});
